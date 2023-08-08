72b5c71df2fa98f3833c6dee7294e93e7612b800
Alexandre Lematre <alexandre.lematre@prestashop.com>
2023-08-08 11:25:16 +0200

    Custom installer + unzipper

--- install-dev/theme/views/header.php
+++ install-dev/theme/views/header.php
@@ -1,5 +1,7 @@
 <!-- Header -->
-<div id="header" class="clearfix">
+<div id="header">
+  <img id="prestashop_logo" src="theme/img/prestashop_logo.svg" width="180" height="auto" alt="PrestaShop" loading="lazy" />
+
   <ul id="headerLinks">
     <?php if (is_array($this->getConfig('header.links'))): ?>
       <?php foreach($this->getConfig('header.links') as $link => $label): ?>
@@ -11,6 +13,4 @@
       <?php endforeach ?>
     <?php endif; ?>
   </ul>
-
-  <div id="PrestaShopLogo">PrestaShop</div>
 </div>
